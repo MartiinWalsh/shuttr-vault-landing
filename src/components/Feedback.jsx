20ede2b9aa882c7c3e14898ea546250c55b2bf63
martinw-dev <martinwalsh.dev@gmail.com>
2023-06-04 20:33:54 +0100

    Delay feedback prompt

--- src/components/Feedback.jsx
+++ src/components/Feedback.jsx
@@ -1,12 +1,22 @@
 import dynamic from 'next/dynamic'
 
 const DynamicSidetab = dynamic(
-  () => import('@typeform/embed-react').then((mod) => mod.Sidetab),
+  () =>
+    new Promise((resolve) => {
+      setTimeout(() => {
+        resolve(import('@typeform/embed-react').then((mod) => mod.Sidetab))
+      }, 5000)
+    }),
   { ssr: false }
 )
 
 export function Feedback() {
   return (
-    <DynamicSidetab id="J839Xt3r" buttonColor="#338FF2" buttonText="Feedback" />
+    <DynamicSidetab
+      id="J839Xt3r"
+      buttonColor="#338FF2"
+      buttonText="Feedback"
+      autoClose={3000}
+    />
   )
 }
